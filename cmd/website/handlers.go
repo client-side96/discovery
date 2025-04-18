9ee16be7ba645b70fa4748567d547542c9e93644
Dominic Frey <tech@dominicfrey.dev>
2025-04-18 17:30:46 +0200

    feat: add layout components and load svgs properly

--- cmd/website/handlers.go
+++ cmd/website/handlers.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	tpl "html/template"
 	"net/http"
+	"os"
 	"text/template"
 )
+
+func LoadSVG(path string) tpl.HTML {
+	b, err := os.ReadFile(path)
+	if err != nil {
+		return ""
+	}
+
+	return tpl.HTML(string(b))
+}
 
 func (app *application) homeHandler(res http.ResponseWriter, req *http.Request) {
 	if req.URL.Path != "/" {
@@ -16,12 +27,18 @@ func (app *application) homeHandler(res http.ResponseWriter, req *http.Request) 
 		return
 	}
 
+	funcMap := make(map[string]any)
+	funcMap["LoadSVG"] = LoadSVG
+
 	files := []string{
 		app.htmlDir + "/home.page.html.tmpl",
 		app.htmlDir + "/page.layout.html.tmpl",
+		app.htmlDir + "/header.component.html.tmpl",
+		app.htmlDir + "/navigation.component.html.tmpl",
+		app.htmlDir + "/footer.component.html.tmpl",
 	}
 
-	tmpl, err := template.ParseFiles(files...)
+	tmpl, err := template.New("home.page.html.tmpl").Funcs(template.FuncMap(funcMap)).ParseFiles(files...)
 	if err != nil {
 		http.Error(res, "Internal error", http.StatusInternalServerError)
 		return
