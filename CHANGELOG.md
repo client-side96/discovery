5d26fe943dd49349c19e0ba2a6dc18f6ffdaea73
Dominic Frey <tech@dominicfrey.dev>
2025-04-18 09:20:40 +0200

    chore: initial v2 commit

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Changelog
-
-# v1.0.0 - 2023-07-08
-
-Initial release of the portfolio website.
--- src/helpers.ts
+++
@@ -1,9 +0,0 @@
-export function getElementById(elementId: string) {
-  const container = document.getElementById(elementId);
-
-  if (!container) {
-    throw new Error(`Could not find container with id: '${container}'`);
-  }
-
-  return container;
-}
