2a6494f1bafb38a4f6afbd78a06c4c55ae300ed7
Dominic Frey <tech@dominicfrey.dev>
2025-04-18 10:22:15 +0200

    feat: ensure that home route is not a catch all

--- cmd/website/main.go
+++ cmd/website/main.go
@@ -11,5 +11,10 @@ func main() {
 }
 
 func homeHandler(res http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/" {
+		http.NotFound(res, req)
+		return
+	}
+
 	res.Write([]byte("Hello world"))
 }
